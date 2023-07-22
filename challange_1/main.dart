8c2b864d39cd88276b76783a53b1279e92448a34
campfire5 <barbaqadzedato3@gmail.com>
2023-07-22 19:17:20 +0400

    feat: added head of the tile

--- challange_1/main.dart
+++ challange_1/main.dart
@@ -52,7 +52,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       width: 200,
                       height: 100,
                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12.5), color: const Color.fromARGB(255, 206, 232, 174)),
+                          borderRadius: BorderRadius.circular(12.5),
+                          color: const Color.fromARGB(255, 206, 232, 174)),
                     ),
                     Align(
                       alignment: Alignment.topRight,
@@ -97,7 +98,8 @@ class CustomShapePainter extends CustomPainter {
         size.width,
         size.height,
       )
-      ..quadraticBezierTo(size.width, size.height - arcDimension, size.width - arcDimension, size.height - arcDimension)
+      ..quadraticBezierTo(size.width, size.height - arcDimension,
+          size.width - arcDimension, size.height - arcDimension)
       ..lineTo(2 * arcDimension, size.height - arcDimension)
       ..quadraticBezierTo(arcDimension + 3, 30, arcDimension, arcDimension)
       ..quadraticBezierTo(arcDimension - 3, 0, 0, 0)
@@ -106,6 +108,67 @@ class CustomShapePainter extends CustomPainter {
         0,
       );
 
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
+
+class TileHeadShapePainter extends CustomPainter {
+  const TileHeadShapePainter({
+    required this.color,
+  });
+
+  final Color color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..style = PaintingStyle.fill;
+
+    const arcDimension = 80.0;
+    const tiltValue = 10;
+
+    final path = Path()
+      ..moveTo(0, 0)
+      ..lineTo(
+        size.width - arcDimension,
+        0,
+      )
+      ..quadraticBezierTo(
+        size.width - arcDimension / 2 - tiltValue,
+        0,
+        size.width - arcDimension / 2,
+        size.height / 2,
+      )
+      ..quadraticBezierTo(
+        size.width - arcDimension / 2 + tiltValue,
+        80,
+        size.width,
+        size.height,
+      )
+      ..lineTo(
+        size.width,
+        size.height,
+      )
+      ..lineTo(
+        0,
+        size.height,
+      )
+      ..quadraticBezierTo(
+        0,
+        0,
+        arcDimension / 2,
+        0,
+      )
+      ..lineTo(
+        0,
+        0,
+      );
     canvas.drawPath(path, paint);
   }
 
